f0e738912f61c2c244a69c1def6cccd1bbc1b185
Viraj Nirbhavane <virajlistenstoday@gmail.com>
2024-02-01 18:16:03 +0530

    talk about why hooks? useState

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  //Why hooks? Don't need to take multiple references in JS to update innerHTMLs at various places.
+  
+  let [counter, setCounter] = useState(0)
+
+  // let counter = 15
+
+  const addValue = () => {
+    setCounter(counter+1)
+  }
+
+  const removeValue = () =>{
+    setCounter(counter - 1)
+  }
+
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+      <button
+      onClick={addValue}
+      >Add value {counter}</button>
+      <br />
+      <button
+        onClick={removeValue}
+      >Remove value</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
