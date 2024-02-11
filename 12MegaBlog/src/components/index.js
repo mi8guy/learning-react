d1870f3581a4c54ff319e4d4f9c442ea441a16ce
Viraj Nirbhavane <virajlistenstoday@gmail.com>
2024-02-11 19:41:04 +0530

    MegaBlog :: Production grade react components

---
+++ 12MegaBlog/src/components/index.js
@@ -0,0 +1,9 @@
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import Container from "./container/Container"
+import Logo from "./Logo"
+import LogoutBtn from "./Header/LogoutBtn"
+import Button from "./Button";
+import Input from "./Input";
+
+export {Header, Footer, Container, Logo, LogoutBtn, Button, Input} 
